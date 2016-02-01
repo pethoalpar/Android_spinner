558bf8512cf7461553c1daac35e2079001a4b6c7
proton <pethoalpar@gmail.com>
2016-02-01 22:09:02 +0200

    Android spinner

---
+++ app/src/main/java/com/test/proton/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.test.proton.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) this.findViewById(R.id.spinner);
+        textView = (TextView) this.findViewById(R.id.textView);
+
+        List<String> list = new ArrayList<>();
+        list.add("ABC");
+        list.add("BCD");
+        list.add("CDE");
+        list.add("DEF");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,list);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+}
